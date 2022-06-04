0643a13b523fe35b91543ecebab3963acdc1b1a1
getwebM <mohs@get-web.online>
2022-06-04 11:54:44 +0200

    wip change interface

---
+++ src/interfaces/Common.Interface.ts
@@ -0,0 +1,54 @@
+export class Device {
+  _id: string;
+  _rev: string;
+  doctype: string;
+  serialNumber: string;
+  name: string;
+  nextChecks: NextCheckValue[];
+  type: string;
+  assignedTo: AssignedValue;
+  status: number;
+  inventoryNumber: string;
+  constructor(
+    serialNumber = '',
+    name = '',
+    nextChecks: NextCheckValue[] = [],
+    type = '',
+    assignedTo = { user: '', userGroup: '', customer: '', type: '' },
+    status = 1,
+    inventoryNumber = ''
+  ) {
+    this._id = '';
+    this._rev = '';
+    this.doctype = 'device';
+    this.name = name;
+    this.nextChecks = nextChecks;
+    this.type = type;
+    this.assignedTo = assignedTo;
+    this.status = status;
+    this.serialNumber = serialNumber;
+    this.inventoryNumber = inventoryNumber;
+  }
+}
+
+export class NextCheckValue {
+  checkDefinitionId: string;
+  nextCheck: Date;
+  constructor(checkDefinitionId: string, nextCheck: Date) {
+    this.checkDefinitionId = checkDefinitionId;
+    this.nextCheck = nextCheck;
+  }
+}
+
+export class AssignedValue {
+  user: string;
+  userGroup: string;
+  customer: string;
+  type: string;
+  constructor(user: string, userGroup: string, customer: string, type: string) {
+    this.user = user;
+    this.userGroup = userGroup;
+    this.customer = customer;
+    this.type = type;
+  }
+}
